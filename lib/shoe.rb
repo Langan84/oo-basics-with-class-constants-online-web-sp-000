b37fedb9b3f1cf8f9940370c0abd8f3807da838d
langan.kg Langan <langan.kg@gmail.com>
2021-04-07 16:04:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,11 +3,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS =[]
+  BRANDS.uniq! =[]
 
   def initialize(brand)
     @brand = brand
-    if brand.include?.uniq!
+    if brand.include?
             BRANDS << brand
     end
   end
